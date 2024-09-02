5ed53f54e3df75256bbf813aa31e66261cc6c2da
Eric970619 <115120630+Eric970619@users.noreply.github.com>
2024-09-02 23:14:39 +0800

    erictool.ps1

--- erictool.ps1
+++ erictool.ps1
@@ -1,8 +1,7 @@
 $downloadUrl = "https://raw.githubusercontent.com/Eric970619/page/main/hello.exe"
 $destinationPath = "C:\Users\Public\Downloads\hello.exe"
 
-$wc = New-Object System.Net.WebClient
-$wc.DownloadFile($downloadUrl, $destinationPath)
+Invoke-WebRequest -Uri $downloadUrl -OutFile $destinationPath
 
 while (-not (Test-Path $destinationPath)) {
     Start-Sleep -Seconds 1
